f80fc007dc38e1440cfafd0663b49418b9f45fce
Aasim2038 <aasim2038y@gmail.com>
2025-12-25 17:21:11 -1000

    fixing large img

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ const authMiddleware = (req, res, next) => {
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.static(__dirname)); 
 
 // --- 3. CLOUDINARY CONFIG (File Upload) ---
